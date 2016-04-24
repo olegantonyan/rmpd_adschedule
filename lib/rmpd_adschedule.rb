ddd054fa1a2a86139efb14d56a29336d545a6889
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2016-04-24 19:35:08 +0200

    use fiddle instead of rust

--- lib/rmpd_adschedule.rb
+++ lib/rmpd_adschedule.rb
@@ -1,45 +1,15 @@
 require 'rmpd_adschedule/version'
 require 'rmpd_adschedule/item'
-
-require 'ffi'
-require 'rake'
+require 'rmpd_adschedule/rust'
 
 module RmpdAdschedule
   def self.say_hello
-    i = Item.new(2, '01.02.2016', '04.02.2016', '10:00:00', '18:00:00', 14)
-    j = Item.new(5, '01.02.2016', '04.02.2016', '9:00:00', '18:00:00', 14)
-    arr = [i, j].map(&:to_hash)
+    arr = (1..1000).map { |i| Item.new(i, '01.02.2016', '04.02.2016', '10:00:00', '18:00:00', 14) }.map(&:to_hash)
     r = Rust.calculate(arr)
     puts r
-  end
-
-  module Rust
-    class << self
-      extend FFI::Library
-      ffi_lib "#{File.dirname(__FILE__)}/../rust/target/release/librmpd_adschedule.so"
-      attach_function :ffi_calculate, [:string], :pointer
-
-      def calculate(data)
-        json = data.to_json
-        result = calculate!(json)
-        JSON.parse(result)
-      end
-
-      private
-
-      def calculate!(input_data)
-        raise TypeError, "wrong argument type #{object.class} (expected String)" unless input_data.is_a? String
-        ptr = ffi_calculate(input_data)
-        result = String.new(ptr.read_string)
-        LibC.free(ptr)
-        result
-      end
-    end
-
-    module LibC
-      extend FFI::Library
-      ffi_lib FFI::Library::LIBC
-      attach_function :free, [:pointer], :void
+    loop do
+      a = Rust.calculate(arr)
+      a
     end
   end
 end
---
+++ lib/rmpd_adschedule/rust.rb
@@ -0,0 +1,35 @@
+require 'fiddle'
+
+module RmpdAdschedule
+  module Rust
+    @lib = Fiddle.dlopen("#{File.dirname(__FILE__)}/../../rust/target/release/librmpd_adschedule.so")
+    @ffi_calculate = Fiddle::Function.new(@lib['ffi_calculate'], [Fiddle::TYPE_VOIDP], Fiddle::TYPE_VOIDP)
+
+    def self.calculate(data)
+      json = data.to_json
+      result = calculate!(json)
+      JSON.parse(result)
+    end
+
+    def self.calculate!(input_data)
+      raise TypeError, "wrong argument type #{object.class} (expected String)" unless input_data.is_a? String
+      ptr = @ffi_calculate.call(input_data)
+      result = ptr.to_s
+      LibC.free(ptr)
+      result
+    end
+
+    module LibC
+      @libc = if RUBY_PLATFORM =~ /_64/
+                Fiddle.dlopen('/lib64/libc.so.6')
+              else
+                Fiddle.dlopen('/lib/libc.so.6')
+              end
+      @libc_free = Fiddle::Function.new(@libc['free'], [Fiddle::TYPE_VOIDP], Fiddle::TYPE_VOID)
+
+      def self.free(ptr)
+        @libc_free.call(ptr)
+      end
+    end
+  end
+end
