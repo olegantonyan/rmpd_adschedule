38f09ec4a0f5dcf00900635bc4d4a24a22731af9
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2016-05-08 20:07:44 +0200

    extconf

--- rust/extconf.rb
+++ rust/extconf.rb
@@ -1,5 +1,5 @@
-# require 'mkmf'
+require 'mkmf'
 # have_library('rmpd_adschedule')
-# create_makefile 'rust/rmpd_adschedule'
+create_makefile 'rust/rmpd_adschedule'
 
 system("cd #{File.dirname(__FILE__)} && cargo build --release")
