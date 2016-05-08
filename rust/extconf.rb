c150e9a21aa8b0bf2c4ee7330a303084cbd290d9
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2016-05-08 20:30:04 +0200

    extconf

--- rust/extconf.rb
+++ rust/extconf.rb
@@ -1,5 +1,7 @@
 require 'mkmf'
 
-create_makefile 'rust/rmpd_adschedule'
+create_makefile 'rmpd_adschedule'
 
-system("cd #{File.dirname(__FILE__)} && cargo build --release")
+makefile = "all:\n  cd rust/ && cargo build --release"
+File.write('Makefile', makefile)
+# system("cd #{File.dirname(__FILE__)} && cargo build --release")
