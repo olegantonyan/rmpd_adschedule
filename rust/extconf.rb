89284974f640574e24911d3a3dc4109c65ffc0d1
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2016-05-09 05:38:15 +0200

    extconf

--- rust/extconf.rb
+++ rust/extconf.rb
@@ -1,5 +1,5 @@
-#require 'mkmf'
+# require 'mkmf'
+# create_makefile 'rmpd_adschedule'
+# File.write('Makefile', "all:\n\tcargo build --release\nclean:\n\trm -rf target\ninstall: ;")
 
-#create_makefile 'rmpd_adschedule'
-
-#File.write('Makefile', "all:\n\tcargo build --release\nclean:\n\trm -rf target\ninstall: ;")
+raise 'I hate you'
