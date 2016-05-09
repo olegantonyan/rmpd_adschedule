669bf0facbfba2a3d3b132376c795f30dd615a8a
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2016-05-09 05:40:36 +0200

    extconf

--- rust/extconf.rb
+++ rust/extconf.rb
@@ -2,4 +2,4 @@
 # create_makefile 'rmpd_adschedule'
 # File.write('Makefile', "all:\n\tcargo build --release\nclean:\n\trm -rf target\ninstall: ;")
 
-raise 'I hate you'
+# raise 'You have to install Rust with Cargo' unless `cargo`
