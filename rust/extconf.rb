5205073a4fb47ced0c40c1d1fd72d7edf46433f8
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2016-05-09 05:43:14 +0200

    Makefile

--- rust/extconf.rb
+++ rust/extconf.rb
@@ -2,4 +2,4 @@
 # create_makefile 'rmpd_adschedule'
 # File.write('Makefile', "all:\n\tcargo build --release\nclean:\n\trm -rf target\ninstall: ;")
 
-# raise 'You have to install Rust with Cargo' unless `cargo`
+raise 'You have to install Rust with Cargo (https://www.rust-lang.org/)' if !system('cargo --version') || system('rustc --version')
