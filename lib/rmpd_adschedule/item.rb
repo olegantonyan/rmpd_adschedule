99292b85d3d4c13bd0abba54bdd2a7e2ee962ea9
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2016-05-11 14:41:55 +0200

    parse date time formats before calc

--- lib/rmpd_adschedule/item.rb
+++ lib/rmpd_adschedule/item.rb
@@ -2,15 +2,18 @@ require 'json'
 
 module RmpdAdschedule
   class Item
+    DATE_FORMAT = '%d.%m.%Y'.freeze
+    TIME_FORMAT = '%H:%M:%S'.freeze
+
     attr_accessor :id, :begin_date, :end_date, :begin_time, :end_time, :playbacks_count
 
     # rubocop: disable Metrics/ParameterLists
     def initialize(id, begin_date, end_date, begin_time, end_time, playbacks_count)
       self.id = id
-      self.begin_date = begin_date
-      self.end_date = end_date
-      self.begin_time = begin_time
-      self.end_time = end_time
+      self.begin_date = begin_date.strftime(DATE_FORMAT)
+      self.end_date = end_date.strftime(DATE_FORMAT)
+      self.begin_time = begin_time.strftime(TIME_FORMAT)
+      self.end_time = end_time.strftime(TIME_FORMAT)
       self.playbacks_count = playbacks_count
     end
     # rubocop: enable Metrics/ParameterLists
